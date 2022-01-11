3b191c4060e087d3e6cc3741ea6cf90762dfb663
Peter Wagenet <peter.wagenet@gmail.com>
2022-01-11 13:46:13 -0800

    Add compiled types

---
+++ components/positioner/content.d.ts
@@ -0,0 +1,18 @@
+import Component from '@glint/environment-ember-loose/glimmer-component';
+export interface PositionerContentSignature {
+    Element: HTMLDivElement;
+    Args: {
+        isHidden: boolean;
+        hideArrow?: boolean;
+    };
+    Yields: {
+        default: [];
+    };
+}
+export default class PositionerContent extends Component<PositionerContentSignature> {
+}
+declare module '@glint/environment-ember-loose/registry' {
+    export default interface Registry {
+        'Positioner::Content': typeof PositionerContent;
+    }
+}
